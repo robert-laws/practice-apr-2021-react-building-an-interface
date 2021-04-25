460e03365a55782aa5b07777c2ba33beeae47a14
Robert Laws <roblaws@me.com>
2021-04-25 13:03:10 +0300

    add react-icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BiArchive } from 'react-icons/bi';
+
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <h1>
+        <BiArchive style={{ fontSize: '2rem' }} />
+        Your Appointments
+      </h1>
+    </div>
+  );
 }
 
 export default App;
